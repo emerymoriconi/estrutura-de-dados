7f7df2c43f8863a8544620b963d3c6baca3edac8
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2023-09-21 11:38:50 -0300

    Initial commit

---
+++ Pilha/pilha.cpp
@@ -0,0 +1,11 @@
+//Lógica LIFO: Last In, First Out
+//Lógica FILO: First In, Last Out
+/*Aplicações:
+    -- Abas no computador: abas no computador são organizadas em pilhas na memória, já que a última aba a ser acessada e fechada é colocada no topo da pilha.
+    -- Botão de voltar: é uma pilha, já que a aba que será retornada é a aba que está no topo da pilha do botão de voltar, a última aba acessada (LIFO). 
+    -- Botão de avançar: é uma pilha, já que quando o botão de voltar é pressionado, a aba que antes era a aba atual, será colocada no topo da pilha do botão de avançar, fazendo dela a aba seguinte a ser acessada.
+    -- Editor de texto: tudo que está sendo feito é colocado no topo da pilha e quando algo for desfeito, a ação no topo da pilha é acessada (Ex: Ctrl + Z).
+*/
+
+//Implementação de Pilha com Vetores
+
